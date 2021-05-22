6471b7d0fe075e55c392c691525e9b07185f8594
Nthabeleng jimson <56711591+jxmson@users.noreply.github.com>
2021-05-22 20:57:06 +0200

    Add files via upload

---
+++ CarpetQuotes/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace CarpetQuotes
+{
+    partial class frmQuotes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbCarpet = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnQuote = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbCarpet
+            // 
+            this.cmbCarpet.FormattingEnabled = true;
+            this.cmbCarpet.Location = new System.Drawing.Point(15, 35);
+            this.cmbCarpet.Name = "cmbCarpet";
+            this.cmbCarpet.Size = new System.Drawing.Size(149, 21);
+            this.cmbCarpet.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Carpet Type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Room dimensions";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Width (m)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Length (m)";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(68, 135);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 5;
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(68, 100);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 6;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(103, 171);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnQuote
+            // 
+            this.btnQuote.Location = new System.Drawing.Point(12, 171);
+            this.btnQuote.Name = "btnQuote";
+            this.btnQuote.Size = new System.Drawing.Size(75, 23);
+            this.btnQuote.TabIndex = 8;
+            this.btnQuote.Text = "Qoute";
+            this.btnQuote.UseVisualStyleBackColor = true;
+            this.btnQuote.Click += new System.EventHandler(this.btnQuote_Click);
+            // 
+            // frmQuotes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 207);
+            this.Controls.Add(this.btnQuote);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbCarpet);
+            this.Name = "frmQuotes";
+            this.Text = "Carpet Quotes";
+            this.Load += new System.EventHandler(this.frmQuotes_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbCarpet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnQuote;
+    }
+}
+
---
+++ CarpetQuotes/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarpetQuotes
+{
+    public partial class frmQuotes : Form
+    {
+        public frmQuotes()
+        {
+            InitializeComponent();         
+
+        }
+
+        private void frmQuotes_Load(object sender, EventArgs e)
+        {
+            cmbCarpet.SelectedIndex = cmbCarpet.Items.Add("Type A (R120 /m^2)");
+            cmbCarpet.Items.Add("Type B (R110 /m^2)");
+            cmbCarpet.Items.Add("Type C (R100 /m^2)");
+            cmbCarpet.Items.Add("Type D (R90 /m^2)");
+            cmbCarpet.Items.Add("Type E (R80 /m^2)");
+            cmbCarpet.Items.Add("Type F (R70 /m^2)");
+            cmbCarpet.Items.Add("Type G (R60 /m^2)");          
+        }
+        double Area(double a, double b)
+        {
+            return a * b;
+        }
+
+        private void btnQuote_Click(object sender, EventArgs e)
+        {
+            double l = double.Parse(txtLength.Text);
+            double w = double.Parse(txtWidth.Text);
+
+            double price = 0;
+
+            if ((string)cmbCarpet.SelectedItem == "Type A (R120 /m^2)")
+                price = Area(l, w) * 120;
+            else if ((string)cmbCarpet.SelectedItem == "Type B (R110 /m^2)")
+                price = Area(l, w) * 110;
+            else if ((string)cmbCarpet.SelectedItem == "Type C (R100 /m^2)")
+                price = Area(l, w) * 100;
+            else if ((string)cmbCarpet.SelectedItem == "Type D (R90 /m^2)")
+                price = Area(l, w) * 90;
+            else if ((string)cmbCarpet.SelectedItem == "Type E (R80 /m^2)")
+                price = Area(l, w) * 80;
+            else if ((string)cmbCarpet.SelectedItem == "Type F (R70 /m^2)")
+                price = Area(l, w) * 70;
+            else if ((string)cmbCarpet.SelectedItem == "Type G (R60 /m^2)")
+                price = Area(l, w) * 60;
+           
+            MessageBox.Show("The price quoted for a carpet " + txtLength.Text + "m by " + txtWidth.Text + "m = R" + price.ToString(),"Carpet Quotation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
